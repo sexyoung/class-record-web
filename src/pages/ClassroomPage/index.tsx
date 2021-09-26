0deefc89087e4b80ca6939dc71e40582c66c1dbe
寫詩羊 <Habuche@gmail.com>
2021-09-27 00:56:29 +0800

    fix(class): page info

--- src/pages/ClassroomPage/index.tsx
+++ src/pages/ClassroomPage/index.tsx
@@ -5,31 +5,7 @@ export const ClassroomPage: FC = () => {
   return (
     <div>
       主頁
-      <div style={{ border: "1px solid black" }}>
-        課程A
-        <br />
-        日期
-        <br />
-        學生甲
-        <br />
-        學生乙
-        <br />
-        學生丙
-      </div>
-      <div style={{ border: "1px solid black" }}>
-        課程B
-        <br />
-        日期
-        <br />
-        學生甲
-        <br />
-        學生乙
-        <br />
-        學生丙
-      </div>
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0"></div>
-        <div>
+      <div>
           <Header />
           主頁
           <div style={{ border: "1px solid black" }}>
@@ -54,9 +30,11 @@ export const ClassroomPage: FC = () => {
             <br />
             學生丙
           </div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-gray-500">You have a new message!</p>
         </div>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0"></div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
       </div>
     </div>
   );
