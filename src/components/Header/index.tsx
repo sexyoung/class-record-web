3e2f3ca4af3baa3ec93d6ad6c0182aec2553935e
寫詩羊 <Habuche@gmail.com>
2021-09-29 00:08:44 +0800

    refactor(Header): Logout function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router';
 
 import { ROUTE } from 'route';
 import { clearApiToken } from 'utils';
@@ -8,17 +7,12 @@ import { clearApiToken } from 'utils';
 import style from "./style.module.scss";
 
 export const Header: FC = () => {
-  const history = useHistory();
-  const logOut = () => {
-    clearApiToken();
-    history.push(ROUTE.HOME);
-  }
 
   return (
     <ul className={style.Header}>
       <li><Link to={ROUTE.CLASS}>CLASS</Link></li>
       <li><Link to={ROUTE.STUDENT}>STUDENT</Link></li>
-      <li onClick={logOut}>LOGOUT</li>
+      <li><Link to={ROUTE.HOME} onClick={clearApiToken}>LOGOUT</Link></li>
     </ul>
   )
 }
