4d53440654b41d2bca1b2f9a619ae310c7c6eff5
Kelly <kellyu621@gmail.com>
2021-10-02 01:15:56 +0800

    feat(StudentPersonalPage): 編輯頁面與編輯功能

--- src/pages/StudentPersonalPage/index.tsx
+++ src/pages/StudentPersonalPage/index.tsx
@@ -1,10 +1,11 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, FormEventHandler as FEH } from 'react';
 import { fetchApi } from "utils";
 import * as API from "api";
 import * as Type from "domain/type/res/student";
 
-export const StudentPersonalPage = () => {
+export const StudentPersonalPage: FC = () => {
   const [student, setStudent] = useState<Type.Student>();
+  const [isEdit, setIsEdit] = useState(false);
   const urlParams = new URLSearchParams(window.location.search);
   const id = urlParams.get('id');
 
@@ -13,14 +14,48 @@ export const StudentPersonalPage = () => {
       .then(setStudent);
   }, []);
 
+  const finishEdit: FEH<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    id && await fetchApi(API.getOneStudent(+id), {
+      method: "patch",
+      withToken: true,
+      body: {
+        data: {
+          name: formData.get("name")
+        }
+      }
+    });
+    id && await fetchApi(API.getOneStudent(+id), {
+      method: "patch",
+      withToken: true,
+      body: {
+        data: {
+          status: formData.get("status")
+        }
+      }
+    });
+    setIsEdit.bind(false);
+  };
+
   return (
     <div>
       個人頁
-      {student &&
+      {!isEdit && student &&
       <div>
         <div>{student.name}</div>
         <div>{student.status}</div>
+        <button onClick={setIsEdit.bind(null, true)}>編輯</button>
       </div>
+      }
+      {isEdit && student &&
+      <form onSubmit={finishEdit}>
+        <input type="text" name="name" defaultValue={student.name} placeholder="name" required />
+        <input type="text" name="status" defaultValue={student.status} placeholder="status" required />
+        <div>{student.status}</div>
+        <button>完成</button>
+      </form>
       }
     </div>
   );
