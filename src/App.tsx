ab1a8859f4bd8b2cbfb751be47d749cc277203f4
寫詩羊 <Habuche@gmail.com>
2021-10-18 23:10:10 +0800

    style(AuthPage)

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,28 @@ import style from './App.module.css';
 function App() {
   return (
     <ProvideAuth>
-      <Router>
-        <Switch>
-          {/* 登入後頁面 */}
-          <PrivateRoute path={ROUTE.ROLLCALL}><Page.RollCallPage /></PrivateRoute>
-          <PrivateRoute path={ROUTE.CLASS_EDIT}><Page.ClassroomEditPage /></PrivateRoute>
-          <PrivateRoute path={ROUTE.CLASS}><Page.ClassroomPage /></PrivateRoute>
-          <PrivateRoute path={ROUTE.STUDENT}><Page.StudentPage /></PrivateRoute>
-          <PrivateRoute path={ROUTE.STUDENTPERSONAL}><Page.StudentPersonalPage /></PrivateRoute>
-          <PrivateRoute path={ROUTE.AUTH}><Page.AuthenticationPage /></PrivateRoute>
+      <div className={style.App}>
+        <div className={style.container}>
+          <Router>
+            <Switch>
+              {/* 登入後頁面 */}
+              <PrivateRoute path={ROUTE.ROLLCALL}><Page.RollCallPage /></PrivateRoute>
+              <PrivateRoute path={ROUTE.CLASS_EDIT}><Page.ClassroomEditPage /></PrivateRoute>
+              <PrivateRoute path={ROUTE.CLASS}><Page.ClassroomPage /></PrivateRoute>
+              <PrivateRoute path={ROUTE.STUDENT}><Page.StudentPage /></PrivateRoute>
+              <PrivateRoute path={ROUTE.STUDENTPERSONAL}><Page.StudentPersonalPage /></PrivateRoute>
+              <PrivateRoute path={ROUTE.AUTH}><Page.AuthenticationPage /></PrivateRoute>
 
-          {/* 登入前頁面 */}
-          <Route exact path={ROUTE.LOGIN}><Page.LoginPage /></Route>
-          <Route exact path={ROUTE.SIGNUP}><Page.SignUpPage /></Route>
-          <Route exact path={ROUTE.HOME}><Page.HomePage /></Route>
-        </Switch>
-      </Router>
-      <div className={style.footer}>
-        © 2021 RoRoCall. 版權所有。
+              {/* 登入前頁面 */}
+              <Route exact path={ROUTE.LOGIN}><Page.LoginPage /></Route>
+              <Route exact path={ROUTE.SIGNUP}><Page.SignUpPage /></Route>
+              <Route exact path={ROUTE.HOME}><Page.HomePage /></Route>
+            </Switch>
+          </Router>
+        </div>
+        <div className={style.footer}>
+          © 2021 RoRoCall. 版權所有。
+        </div>
       </div>
     </ProvideAuth>
   );
--- src/pages/AuthenticationPage/index.tsx
+++ src/pages/AuthenticationPage/index.tsx
@@ -1,8 +1,15 @@
+import cx from 'classnames';
+import * as Icon from '@heroicons/react/outline';
+import { Link, useHistory } from "react-router-dom";
 import { FC, FormEventHandler as FEH } from "react";
-import { useHistory } from "react-router";
-import { ROUTE } from "route";
+
 import * as API from "api";
+import { ROUTE } from "route";
 import { fetchApi } from "utils";
+
+import style from './style.module.css';
+import btnStyle from 'components/btn.module.css';
+import comStyle from 'components/common.module.css';
 
 export const AuthenticationPage: FC = () => {
   const history = useHistory();
@@ -21,11 +28,24 @@ export const AuthenticationPage: FC = () => {
   };
 
   return (
-    <div>
-      <div>認證頁</div>
+    <div className={style.AuthenticationPage}>
+      <Link to={ROUTE.HOME}>
+        <Icon.ClipboardCheckIcon className={cx(comStyle.logo, style.logo)} />
+      </Link>
+      <div className={style.title}>信箱認證</div>
       <form onSubmit={auth}>
-        <input type="text" name="code" placeholder="四位驗證碼" required />
-        <button>驗證</button>
+        <div className="flex flex-col mb-9">
+          <input
+            type="text"
+            name="code"
+            id="code"
+            required
+            placeholder="認證碼"
+            className="shadow appearance-none rounded-sm w-full py-2 px-3  text-lavender-700 mt-1 leading-tight focus:outline-none focus:ring ring-lavender-700 focus:bg-lavender-200"
+          />
+          <div>沒收到? <span>點此重發(還沒做)</span></div>
+        </div>
+        <button className={cx(btnStyle.btn, "font-bold text-lg hover:bg-lavender-700")}>驗證</button>
         {/* 沒token導到登入頁 */}
       </form>
     </div>
