9d66113a7198a988467bd87ba145bdda7b44ebd8
寫詩羊 <Habuche@gmail.com>
2021-10-26 01:55:50 +0800

    style(ClassroomPage): append color

--- src/pages/ClassroomPage/index.tsx
+++ src/pages/ClassroomPage/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import { FC, useEffect, useState } from "react";
+import * as Icon from '@heroicons/react/solid';
 
 import * as API from "api";
+import { ROUTE } from "route";
 import * as Comp from "components";
 import * as Type from "domain/type/res/classroom";
-import { Link } from "react-router-dom";
-import { ROUTE } from "route";
+
+import style from './style.module.css';
 
 export const ClassroomPage: FC = () => {
   const [classroomList, setClassroomList] = useState<Type.Class[]>();
@@ -16,21 +19,21 @@ export const ClassroomPage: FC = () => {
   }, []);
 
   return (
-    <div>
+    <div className={style.ClassroomPage}>
       <Comp.Header />
-      課程頁
-      <p>
-        <Link to={ROUTE.ROLLCALL}>點名</Link>
-      </p>
       {classroomList && classroomList.map(classroom =>
-        <div key={classroom.id}>
-          <Link to={`${ROUTE.CLASS_EDIT}/?id=${classroom.id}`}>EDIT</Link><br />
-          {classroom.date}<br />
+        <div key={classroom.id} className={style.classroom}>
+          <div className={style.title}>
+            <span className={style.date}>{classroom.date.slice(5, 16)}</span>
+            <span>{classroom.students.length}人</span>
+            <Link to={`${ROUTE.CLASS_EDIT}/?id=${classroom.id}`}>EDIT</Link><br />
+          </div>
           {classroom.students.map(student =>
-            <span key={student.id}>{student.name}</span>
+            <span key={student.id} className={style.student}>{student.name}</span>
           )}
         </div>
       )}
+      <Link className={style.add} to={ROUTE.ROLLCALL}><Icon.PlusCircleIcon color="#926C9A" /></Link>
     </div>
   );
 };
