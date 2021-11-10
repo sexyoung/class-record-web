0bdf1e13530432f18014970ff6ffaaba5b09d6d7
sexyoung <Habuche@gmail.com>
2021-11-10 11:19:36 +0800

    feat(ImportPage): use sheetJS to import excel file

--- src/pages/ImportPage/index.tsx
+++ src/pages/ImportPage/index.tsx
@@ -1,9 +1,22 @@
+import XLSX from 'xlsx';
+import React from "react";
+
 import * as Comp from "components";
 export const ImportPage = () => {
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = async ({ currentTarget }) => {
+    const [ file = null ] = currentTarget.files as FileList;
+    if(!file) return;
+    const data = await file.arrayBuffer();
+    const workbook = XLSX.read(data);
+    console.log(XLSX.utils.sheet_to_json(workbook.Sheets.Sheet1));
+  };
+
   return (
     <div>
       <Comp.Header />
       ImportPage
+      <input type="file" onChange={handleChange} />
     </div>
   );
 };
