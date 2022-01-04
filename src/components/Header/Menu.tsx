595ea43eabddf8904d906282f6608bfb3ba35b85
寫詩羊 <Habuche@gmail.com>
2022-01-05 02:11:05 +0800

    feat(Menu): 選單使用動畫, 並把Menu從Header拆出來

---
+++ src/components/Header/Menu.tsx
@@ -0,0 +1,47 @@
+import cx from "classnames";
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import * as Icon from '@heroicons/react/outline';
+
+import { ROUTE } from "route";
+import { useAuth } from "hooks";
+import { clearApiToken } from 'utils';
+import style from "./Menu.module.css";
+
+interface IMenu {
+  isShow: boolean;
+  onClose: () => void;
+}
+
+export const Menu: FC<IMenu> = (props) => {
+  const auth = useAuth();
+  return (
+    <div className={cx(style.Menu, {[style.show]: props.isShow})}>
+      <div className={style.buttonContainer}>
+        <div onClick={props.onClose} className={style.menuButton} aria-expanded="false">
+          <Icon.XIcon className="w-5 h-5" />
+        </div>
+      </div>
+      <div className={style.profileContainer}>
+        <div className={style.img} />
+        {auth.teacher &&
+          <div>
+            <div className={style.title}>{auth.teacher.name}</div>
+            <div className={style.email}>{auth.teacher.email}</div>
+          </div>
+        }
+      </div>
+      <div>
+        <Link to={ROUTE.IMPORT} className={style.menuItem}>匯入資料</Link>
+        <Link to={ROUTE.PASSWD} className={style.menuItem}>更改密碼</Link>
+        <div className={style.menuItem}>老師請假</div>
+
+        <div className={style.menuItem}>
+          <Link to={ROUTE.HOME} onClick={clearApiToken}>
+            登出
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,18 @@
-import { FC, useState } from 'react';
 import cx from 'classnames';
+import { FC, useState } from 'react';
 import * as Icon from '@heroicons/react/outline';
 import { Link, useHistory } from 'react-router-dom';
 
+import { Menu } from './Menu';
 import { ROUTE } from 'route';
-import { useAuth } from 'hooks';
-import { clearApiToken } from 'utils';
 
 import style from "./style.module.css";
 import comStyle from 'components/common.module.css';
 
 export const Header: FC = () => {
-  const auth = useAuth();
-  console.log("auth",auth);
-
   const history = useHistory();
   const pathname = history.location.pathname;
-  const [isMenu, setIsMenu] = useState(false);
+  const [isShowMenu, setIsShowMenu] = useState(false);
 
   return (
     <nav className={style.Header}>
@@ -33,47 +29,11 @@ export const Header: FC = () => {
             </div>
           </div>
 
-          <button onClick={setIsMenu.bind(null, !isMenu)} type="button" className="bg-lavender-200 rounded-md inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+          <button onClick={setIsShowMenu.bind(null, true)} type="button" className="bg-lavender-200 rounded-md inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
             <Icon.MenuIcon className="w-5 h-5" />
           </button>
 
-          {isMenu &&
-          <div className={style.menuContainer}>
-            {/* <div className={style.menuContent}> */}
-            <div className={style.buttonContainer}>
-              <div onClick={setIsMenu.bind(null, !isMenu)} className={style.menuButton} aria-expanded="false">
-                <Icon.XIcon className="w-5 h-5" />
-              </div>
-            </div>
-            <div className={style.profileContainer}>
-              <div className={style.img} />
-              {auth.teacher &&
-                  <div>
-                    <div className={style.title}>{auth.teacher.name}</div>
-                    <div className={style.email}>{auth.teacher.email}</div>
-                  </div>
-              }
-            </div>
-            <div>
-              {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-              <Link to={ROUTE.IMPORT} className={style.menuItem}>匯入資料</Link>
-              <Link to={ROUTE.PASSWD} className={style.menuItem}>更改密碼</Link>
-              <div className={style.menuItem}>老師請假</div>
-
-              <div className={style.menuItem}>
-                <Link
-                  to={ROUTE.HOME}
-                  onClick={clearApiToken}
-                >
-                  {/* <Icon.LogoutIcon className="w-5 h-5 text-lavender-500 hover:text-white" /> */}
-                    登出
-                </Link>
-              </div>
-            </div>
-            {/* </div> */}
-          </div>
-          }
-
+          <Menu isShow={isShowMenu} onClose={setIsShowMenu.bind(null, false)} />
         </div>
       </div>
 
